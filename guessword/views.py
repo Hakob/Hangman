6c551ff7c2522403cbcd91a16bf6e72646cae015
Hakob <hakoaraqelyan@gmail.com>
2018-04-10 00:39:43 +0400

    Second working state ver.2.0

--- guessword/views.py
+++ guessword/views.py
@@ -8,22 +8,33 @@ from django.contrib import messages
 # Imports from your apps
 from .models import WordsModel
 
+GAME_OVER_MSG = "Your chances have expired"
+WINNER_MSG = "You have guessed the word!!!"
+
 CHANCES = 7
 WORD_IS_GUESSED = -1
 LETTER_IS_NOT_IN_WORD = -2
 YOUR_CHANCES_HAVE_EXPIRED = None
 
 
+def initialize_session(request):
+    request.session['chances'] = CHANCES
+    current_word = WordsModel.words.random()
+    current_word = str(current_word)
+    word_letters = list(enumerate(current_word))
+    request.session['word_letters'] = word_letters
+    return len(current_word)
+
+
+def clear_session(request):
+    request.session.flush()
+
+
 class GuessWordView(View):
     template_name = 'game.html'
 
     @staticmethod
-    def check_if_letter_in_word(request, letter):
-        if not request.session['chances']:
-            return YOUR_CHANCES_HAVE_EXPIRED
-
-        if not request.session['word_letters']:
-            return WORD_IS_GUESSED
+    def check_if_letter_is_in_word(request, letter):
 
         matched_indexes = []
         for idx, item in enumerate(request.session['word_letters']):
@@ -31,46 +42,28 @@ class GuessWordView(View):
                 matched_indexes.append(item[0])
                 request.session['word_letters'].pop(idx)
 
+        if not request.session['word_letters']:
+            clear_session(request)
+            messages.add_message(request, messages.SUCCESS, WINNER_MSG)
+            return render(request, 'game.html')
+
         if matched_indexes:
-            return matched_indexes
+            return JsonResponse({'failed': 0, 'indexes': matched_indexes})
 
         request.session['chances'] -= 1
-        return LETTER_IS_NOT_IN_WORD
-
-    @staticmethod
-    def clear_session(request):
-        request.session.flush()
-
-    @staticmethod
-    def initialize_session(request):
-        request.session['chances'] = CHANCES
-        current_word = WordsModel.words.random()
-        current_word = str(current_word)
-        word_letters = list(enumerate(current_word))
-        request.session['word_letters'] = word_letters
-        return len(current_word)
+        if not request.session['chances']:
+            clear_session(request)
+            messages.add_message(request, messages.INFO, GAME_OVER_MSG)
+            return render(request, 'game.html')
+        return JsonResponse({'failed': 1})
 
     def get(self, request):
-        self.clear_session(request)
-        count = self.initialize_session(request)
+        clear_session(request)
+        count = initialize_session(request)
         context = {'letters_count': list(range(count))}
         return render(request, self.template_name, context=context)
 
     def post(self, request):
         letter = request.POST.get('letter')
-        status = self.check_if_letter_in_word(request, letter)
-
-        if status == YOUR_CHANCES_HAVE_EXPIRED:
-            self.clear_session(request)
-            messages.add_message(request, messages.ERROR, "Your chances have expired")
-            return render(request, 'results.html')
-
-        elif status == WORD_IS_GUESSED:
-            self.clear_session(request)
-            messages.add_message(request, messages.SUCCESS, "You have guessed the word!!!")
-            return render(request, 'results.html')
-
-        elif status == LETTER_IS_NOT_IN_WORD:
-            return JsonResponse({'failed': 1})
-        else:
-            return JsonResponse({'failed': 0, 'indexes': status})
+        response = self.check_if_letter_is_in_word(request, letter)
+        return response
